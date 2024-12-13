de7a6e08d390a3b21afcc8edb46d372c486f5b6a
DevAttaKhan <attakhan220@gmail.com>
2024-12-14 00:16:24 +0500

    testing

--- src/components/sections/home/banner.tsx
+++ src/components/sections/home/banner.tsx
@@ -5,13 +5,13 @@ import windTurbine from "@/assets/lottie/wind-turbine-white.json";
 const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
 export const Banner = () => {
   return (
-    <section className="gradient-animation h-screen  md:flex items-center py-28 md:py-52 lg:py-[calc(208px_+_88px)] overflow-hidden">
-      <div className="container flex flex-col gap-12 md:flex-row items-center justify-between">
+    <section className="gradient-animation relative h-screen overflow-hidden">
+      <div className="container flex flex-col   ">
         <div className="left max-w-[400px] md:max-w-[660px] ">
-          <h1 className="text-white text-center md:text-left text-2xl md:text-3xl lg:text-5xl font-primary font-bold mb-4">
+          <h1 className="text-white text-center md:text-left text-3xl md:text-4xl lg:text-5xl font-primary font-bold mb-4">
             {BANNER.heaing}
           </h1>
-          <p className=" text-sm text-center md:text-left md:text-xl lg:text-2xl font-secondary text-white">
+          <p className=" text-base text-center md:text-left md:text-xl lg:text-2xl font-secondary text-white">
             {BANNER.subheading}
           </p>
         </div>
@@ -20,7 +20,7 @@ export const Banner = () => {
           loop
           play
           animationData={windTurbine}
-          className="wind-turbine shrink-0 w-right-clamp"
+          className="wind-turbine shrink-0 w-right-clamp absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
         />
       </div>
     </section>
